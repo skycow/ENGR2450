3482f794df05dbfff3225cc3b7569dd17a467233
Skyler Cowley <skyler.cowley@gmail.com>
2015-03-23 18:37:18 -0600

    Update Problem2.cpp

--- A5/Problem2/Problem2.cpp
+++ A5/Problem2/Problem2.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main(){
    cout<<"This is the program for problem 2"<<endl;
+   cout<<"I realized I probably don't need this since problem one and two are so similar";
    return 0;
 }
