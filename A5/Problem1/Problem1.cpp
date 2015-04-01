f98acc8f248bf3050e6e6c9b909684350e511077
Skyler Cowley <skyler.cowley@gmail.com>
2015-04-01 08:53:49 -0600

    Problem 1 & 2 coded

--- A5/Problem1/Problem1.cpp
+++ A5/Problem1/Problem1.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 double f(double);
+double g(double);
 double Trapm(double, int, double[]);
 
 int main(){
-   double a, b, y[20];
+   double a, b, *y;
    int n;
    cout<<"Enter the value of a: ";
    cin>>a;
@@ -14,6 +15,9 @@ int main(){
    cin>>b;
    cout<<"Enter the value of n: ";
    cin>>n;
+
+   y = new double[n];
+
    double h = (b-a)/n;
    for(int i = 0; i < n; i++){
       y[i] = f(a+i*h);
@@ -32,12 +36,16 @@ double Trapm(double h, int n, double f[]){
    for(int i = 1; i < n-1; i++){
       sum = sum + 2 * f[i];
    }
-   sum = sum + f[n];
-   return h + sum/2;
+   sum = (sum + f[n-1]);
+   return (h + sum/2);
 
 }
 
 double f(double x){
+	return sqrt(9.8*68.1 / 0.25)*tanh(sqrt(9.8*0.25 / 68.1)*x);
+}
+
+double g(double x){
    double pi = 3.141592654;
    double sum = 0.0;
    for(int j =0; j <= 5; j++){
---
+++ VSProblem2/VSProblem2/Problem2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+double Trapm(double, int, double[]);
+
+int main(){
+	double a, b, *y;
+	int n;
+	cout << "Enter the value of a: ";
+	cin >> a;
+	cout << "Enter the value of b: ";
+	cin >> b;
+	cout << "Enter the value of n: ";
+	cin >> n;
+
+	y = new double[n+1];
+
+	double h = (b - a) / n;
+	for (int i = 0; i < n+1; i++){
+		cout << "Enter value " << i + 1 << ":";
+		cin >> y[i];
+	}
+	double I = Trapm(h, n, y);
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	cout << "n: " << n << endl;
+	cout << "h: " << h << endl;
+	cout << "I: " << I << endl;
+	return 0;
+}
+
+double Trapm(double h, int n, double f[]){
+	double sum = f[0];
+	for (int i = 1; i < n - 1; i++){
+		sum = sum + 2 * f[i];
+	}
+	sum = (sum + f[n - 1]);
+	return (h + sum / 2);
+
+}
+
+
